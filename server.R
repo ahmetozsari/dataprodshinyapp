e59b8706e05505f61a940c11fab0c44e1d7ff929
ahmetozsari <ahmetozsarieng@gmail.com>
2020-11-12 18:08:26 +0300

    Update server.R

--- server.R
+++ server.R
@@ -1,28 +1,31 @@
-ls#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+# Coursera - Developing Data Products- Course Project
+
+# server.R file for the shiny app
+
+# This app was developed to help people choose the best car for their trip, 
+# using mtcars dataset, from [R]
 
 library(shiny)
+library(datasets)
+library(dplyr)
 
-# Load Data
-
-prpeople <- read.csv("./puertoricansinusa.csv")
-
-# Define a server for the Shiny app 
- function(input, output) { 
+shinyServer(function(input, output) {
+  
+  # Show the cars that correspond to the filters
+  
+  output$table <- renderDataTable({
     
-   # Fill in the spot we created for a plot 
-   output$people <- renderPlot({ 
-      
-     # Render a barplot 
-     barplot(prpeople[,input$Region],  
-             main=input$Region, 
-             ylab="Number of Puerto Rican in USA", 
-             xlab="Year") 
-   })
- }
+    hp_seq <- seq(from = input$hp[1], to = input$hp[2], by = 1)
+    avg_sq <- seq(from = input$avg[1], to = input$avg[2], by = .1)
+    
+    data <- transmute(mtcars, Car = rownames(mtcars), Milage = mpg, 
+                      Cylinders = cyl, Horsepower = hp, 
+                      Transmission = am, Gears=gear, Quater_Mile_Time= qsec)
+    data <- filter(data, Milage %in% avg_sq, Cylinders %in% input$cyl,Horsepower %in% hp_seq, Transmission %in% input$am, Gears %in% input$gear)
+    data <- mutate(data, Transmission = ifelse(Transmission==0, "Automatic", "Manual"))
+    
+    data
+    
+  }, options = list(lengthMenu = c(5, 15, 30), pageLength = 30))
+  
+})
